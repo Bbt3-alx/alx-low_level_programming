68ea9cc15f9fe9bbe73d4ff3bff0394a4f5e4ea3
Brehyma Traore <brehymatraore50@gmail.com>
2023-02-17 22:47:28 -0800

    Issue fix commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - Program start
  * Return: Program end
@@ -14,7 +15,7 @@ int main(void)
 	{
 		putchar(lower_al);
 		upper_al = lower_al;
-		putchar(upper_al);
+		putchar(toupper(upper_al));
 	}
 	putchar('\n');
 	return (0);

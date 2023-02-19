54c0bd922011180e24a2a78074341514a1373527
Brehyma Traore <brehymatraore50@gmail.com>
2023-02-18 17:28:09 -0800

    Issue fix commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program start
+ * Return: end program
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}

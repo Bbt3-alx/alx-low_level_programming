f51b25ddcf61cba4c808d9e715e4c59403c55060
Brehyma Traore <brehymatraore50@gmail.com>
2023-02-19 08:50:27 -0800

    Issue fix commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - the program starter point
+ *Return: The program end point
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+/**
+ **main - the program starter point
+ **Return: The program end point
+ **/ 
+
+int main(void)
+{
+	printf("%du %cu %fu  \n", 10, "dix", "1.0");
+	return (0);
+}

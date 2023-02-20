25d12def095828c89054808fb57d77b08cd44ad8
Brehyma Traore <brehymatraore50@gmail.com>
2023-02-20 11:17:35 -0800

    Issue fix commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,13 @@ int main(void)
 	int myInt;
 	char myChar;
 	double myDouble;
+	float myFloat;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(myInt));
 	printf("Size of a char: %lu byte(s\n", (unsigned long) sizeof(myChar));
-	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(myDouble));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(myInt));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(myInt));
+	printf("Size of a long long int: %lu byte(s)\n",
+			(unsigned long) sizeof(myDouble));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(myFloat));
 	return (0);
 }

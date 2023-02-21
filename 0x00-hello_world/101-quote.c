baa7788e26cee7a8c65a2434769955fb4b69c289
Brehyma Traore <brehymatraore50@gmail.com>
2023-02-21 03:58:35 -0800

    Issue fix commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - This programe will print exactly the tex bellow
+ * and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}

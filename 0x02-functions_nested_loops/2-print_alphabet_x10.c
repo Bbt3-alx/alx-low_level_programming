6b97489593eeaebd3b9defb99cb25eec415c47dd
Brehyma Traore <brehymatraore50@gmail.com>
2023-02-23 02:09:01 -0800

    Issue fix commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,21 +2,23 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - check the code.
  *
  * Return: always 0.
  */
 void print_alphabet_x10(void)
 {
 	int i, alpha_time;
-	alpha_time = 1;
-	for (alpha_time=0; alpha_time<=10; alpha_time++)
+
+	alpha_time = 0;
+
+	for (alpha_time = 0; alpha_time < 10; alpha_time++)
 	{
-		for (i='a'; i<='z'; i++)
+		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}
-	
+
 	_putchar('\n');
 	}
 }

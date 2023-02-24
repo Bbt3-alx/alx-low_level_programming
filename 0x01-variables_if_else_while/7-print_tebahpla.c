eafe35f24292d47a2f867a84668e6f36870e74b4
Brehyma Traore <brehymatraore50@gmail.com>
2023-02-24 02:23:20 -0800

    Issue fix commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - that program print lowercase alphabet in reverse.
+ * Followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alpha;
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}

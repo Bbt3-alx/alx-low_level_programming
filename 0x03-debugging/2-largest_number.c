2d97da2d18b5944804e2f9e57dbdc9985e531114
Brehyma Traore <brehymatraore50@gmail.com>
2023-02-24 16:10:39 -0800

    Issue fix commit

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the largest of 3 integers
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	a = 3;
+	b = 5;
+	c = 7;
+
+	largest_number(a, b, c);
+
+	return (0);
+}

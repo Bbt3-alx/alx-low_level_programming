0f9ae81ba8daec1c5cfd57bca70a8c694ac1a369
Brehyma Traore <brehymatraore50@gmail.com>
2023-02-26 06:45:28 -0800

    Issue fix commit

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -12,6 +12,10 @@ int main(void)
 	int month = 2;
 	int day = 29;
 
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
 	print_remaining_days(month, day, year);
 
 	return (0);

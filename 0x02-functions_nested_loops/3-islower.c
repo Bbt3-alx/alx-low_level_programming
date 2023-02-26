be5106acf12617a1251a48603a4b2e3826c48d6d
Brehyma Traore <brehymatraore50@gmail.com>
2023-02-26 15:39:51 -0800

    Issue fix commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,11 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * main - check the code.
-
+ * _islower - check the code.
+ *
+ * @c: charactere to check
+ * @return 1 if lower, 0 else
+ *
  * Return: Always return 0.
  */
 int _islower(int c)

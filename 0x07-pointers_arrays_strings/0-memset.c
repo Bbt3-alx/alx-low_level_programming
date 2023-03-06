f2822ddc266b62ae43d628a175e1488ac8a6142a
Brehyma Traore <brehymatraore50@gmail.com>
2023-03-06 11:11:07 -0800

    BETTY ERROR: Does not appear to be a unified-diff format patch. Fixed

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * _memset - This function fills the first n bytes of the memory area.
  * @s: Thi param point to a memory area
- * @b: This param is the constant byte to be fill in the 
+ * @b: This param is the constant byte to be fill in the
  * Memory area ponted by the param s.
- * @n: This param is the first n bytes of the memory area 
+ * @n: This param is the first n bytes of the memory area
  * pointed by s.
+ * Return: A string with the new memory byte of s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {

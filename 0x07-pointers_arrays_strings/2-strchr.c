dfced2078e257d27a1e537da3f99b50aa531bb7c
Brehyma Traore <brehymatraore50@gmail.com>
2023-03-07 02:17:35 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,10 +12,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i, len;
 
+	len = strlen(s);
 	i = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; i <= len; i++)
 	{
 		if (s[i] == c)
 		{

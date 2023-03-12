9a39e95b2a846fd065d408c0af608cc3c9c3e799
Brehyma Traore <brehymatraore50@gmail.com>
2023-03-12 16:51:11 -0700

    Write a program that multiplies two numbers.

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,14 +16,14 @@ int main(int argc, char **argv)
 	for (i = 1; i < argc; i++)
 	{
 		int x = strtol(argv[i], NULL, 10);
-		if (argc == 1)
-		{
 
-			printf("%d\n", argc);
-			printf("Error\n");
-			return (1);
-		}
+
 		result = result * x;
+	}
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
 	}
 	printf("%d\n", result);
 	return (0);

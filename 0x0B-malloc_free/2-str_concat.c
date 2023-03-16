a59980fcc4e5ac72bf003978bccfebdd39dcab4c
Brehyma Traore <brehymatraore50@gmail.com>
2023-03-15 17:10:34 -0700

    Issue fexed 1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,18 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *cat = malloc(strlen(s1) + strlen(s2) + 1);
-
+	if (cat == NULL)
+	{
+		return (NULL);
+	}
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	strcpy(cat, s1);
 	strcat(cat, s2);
 	return (cat);

fbeeaa39004bb381653594e464ae7dbb8272e598
Brehyma Traore <brehymatraore50@gmail.com>
2023-03-17 19:05:25 -0700

    Write a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,36 +3,44 @@
 #include <string.h>
 
 /**
- * string_nconcat - This function concatenate two strings
- * @s1: The first string
- * @s2: The second string
- * @n: The number of byte of s2 to be concatenate to s1.
- * Return: A char pointer.
+ * string_nconcat - concatenate two string
+ * @s1: String 1;
+ * @s2: String 2;
+ * @n: The number of bytes to be concatenate.
+ * Return: A char pointer to the new memory location.
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str_cat;
-	size_t len;
-	unsigned int i;
+char *str;
+unsigned int i, len1 = 0, len2 = 0;
 
-	/* Allocate the required memory space for concatenation. */
-	str_cat = malloc(strlen(s1) + n + 1);
-	/* Verify if the allocation is done. */
-	if (str_cat == NULL)
-	{
-		printf("Impossible to allocate memory");
-		return (NULL);
-	}
-	strcpy(str_cat, s1);
-	len = strlen(str_cat);
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	for (i = 0; i < n && s2[i] != '\0'; i++)
-	{
-		str_cat[len + i] = s2[i];
-	}
-	str_cat[len + i] = '\0';
-	return (str_cat);
+while (s1 && s1[len1])
+len1++;
 
+while (s2 && s2[len2])
+len2++;
+
+
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+if (n > len2)
+n = len2;
+
+str = malloc((len1 + n + 1) * sizeof(char));
+if (str == NULL)
+return (0);
+
+for (i = 0; i < len1; i++)
+str[i] = s1[i];
+
+for (; i < (len1 + n); i++)
+str[i] = s2[i - len1];
+
+str[i] = '\0';
+return (str);
+}

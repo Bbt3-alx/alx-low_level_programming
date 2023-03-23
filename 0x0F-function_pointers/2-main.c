493c881377e1b064693e78d44c5449e2067ebd1b
Brehyma Traore <brehymatraore50@gmail.com>
2023-03-22 23:39:31 -0700

    A bit fix

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * is_98 - check if a number is equal to 98
+ * is_98 - check if a number is equal to 99.
  * @elem: the integer to check
  *
  * Return: 0 if false, something else otherwise.
@@ -41,7 +41,7 @@ int abs_is_98(int elem)
  */
 int main(void)
 {
-	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, '\n'2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
 	int index;
 
 	index = int_index(array, 20, is_98);

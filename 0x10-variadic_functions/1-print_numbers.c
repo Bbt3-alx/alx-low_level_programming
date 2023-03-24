e8eacf5c90b6c8aafb270ab855c195129445a411
Brehyma Traore <brehymatraore50@gmail.com>
2023-03-23 23:37:17 -0700

    A  function that prints strings, followed by a new line.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,21 +14,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		int p = va_arg(ap, unsigned int);
 
-		if (separator == NULL)
-		{
-			continue;
-		}
-		if (i == n)
+		if (separator == NULL || i == 0)
 		{
 			printf("%d", p);
 		}
 		else
 		{
-			printf("%d%s", p, separator);
+			printf("%s%d", separator, p);
 		}
 	}
 	printf("\n");
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings -  prints strings, followed by a new line.
+ * @separator: separator.
+ * @n: number of argument
+ * Return: Nothing.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		char *p = va_arg(ap, char *);
+
+	
+		if (p == NULL)
+		{
+			printf("%p", NULL);
+		}
+		else if (separator == NULL || i == 0)
+		{
+			printf("%s", p);
+		}
+		else
+		{
+			printf("%s%s", separator, p);
+		}
+	}
+	printf("\n");
+}

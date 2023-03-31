7d97bb06eb25aecd14042456c727e3c315e191d6
Brehyma Traore <brehymatraore50@gmail.com>
2023-03-31 08:59:58 -0700

    Return NULL if the allocation for the new node fai

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,7 +9,11 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = (list_t *)malloc(sizeof(list_t));
-
+	
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = *head;

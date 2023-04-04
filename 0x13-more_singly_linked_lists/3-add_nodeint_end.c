c08a8297cc6e4e4142db0da8fbf7c5a7957c90df
Brehyma Traore <brehymatraore50@gmail.com>
2023-04-04 05:03:17 -0700

    Betty code style ERROR: trailing whitespace; WARNING: Missing a blank line after declarations

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,8 +10,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new = (listint_t *)malloc(sizeof(listint_t));
 	listint_t *temp;
+
 	temp = *head;
-	
+
 	if (new == NULL)
 	{
 		return (NULL);
@@ -25,7 +26,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		*head = new;
 		return (*head);
-	}	
+	}
 	while (temp->next != NULL)
 	{
 		temp = temp->next;

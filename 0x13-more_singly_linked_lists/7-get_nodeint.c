e07a346f53251af344ac9181e408cae42fc618ea
Brehyma Traore <brehymatraore50@gmail.com>
2023-04-05 04:45:22 -0700

    Betty code style error fixed

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,17 +10,17 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int nb = 0;
 	listint_t *temp;
-	
-	temp = head;
 
-	while (temp->next != NULL && nb < index)
-	{
-		temp = temp->next;
-		nb++;
-	}
+	temp = head;
 	if (temp == NULL)
 	{
 		return (NULL);
+	}
+
+	while (temp != NULL && index != nb)
+	{
+		temp = temp->next;
+		nb++;
 	}
 	return (temp);
 }

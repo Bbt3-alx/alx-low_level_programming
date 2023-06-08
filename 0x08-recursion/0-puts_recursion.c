12f16cc8c3365be065eaa6be750397e75c188f6e
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-06-08 01:46:56 -0700

    This is the first commit of the Recurssion project

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ * @s The string to be printed.
+ * Return: Nothing.
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}

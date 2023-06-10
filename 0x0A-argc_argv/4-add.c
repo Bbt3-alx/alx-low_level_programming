01daf0a1e2c03c5b90a99f21a825ea342d670b2c
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-06-09 19:54:42 -0700

    argc argv add number

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,12 +11,12 @@
 int main(int argc, char *argv[])
 {
 	int i, result = 0;
-	
+
 	for (i = 1; i < argc; i++)
 	{
 		result += atoi(argv[i]);
-		
-	}		
+
+	}
 	printf("%d\n", result);
 
 	return (0);

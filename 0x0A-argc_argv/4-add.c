c6947e9f514e87011c87f46ecf9dcdf4a28f2537
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-06-11 18:44:01 -0700

    Hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,10 +11,21 @@
 int main(int argc, char *argv[])
 {
 	int i, result = 0;
+	char *endptr;
 
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		result += atoi(argv[i]);
+		result += strtol(argv[i], &endptr, 10);
+		if (*endptr != '\0' && *endptr != ' ')
+		{
+			printf("Error\n");
+			return (1);
+		}
 
 	}
 	printf("%d\n", result);

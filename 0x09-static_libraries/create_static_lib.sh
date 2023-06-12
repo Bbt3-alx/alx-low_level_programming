c2fda33f88eb5a8258ac21033047062a4c383b60
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-06-11 19:08:10 -0700

    gcc command added to the script

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-ar -rc liball.a *.c
-ranlib liball.a
 
+# This command will compile all .c files to objects .o files'
+gcc -c *.c
+
+# This one will archive objects .o file in a single static library
+ar -rcs liball.a *.c

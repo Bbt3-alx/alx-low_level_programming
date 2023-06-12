f30e00bde97556de123caef449fd3726c42340d5
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-06-11 17:47:12 -0700

    Hot fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,13 @@
  */
 int main(int argc, char **argv)
 {
-	int result;
+	int result = 0;
 
-	if (argc < 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
-	}
+	}	
 	result = atoi(argv[1]) * atoi(argv[2]);
 
 	printf("%d\n", result);

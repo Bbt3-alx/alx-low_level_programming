ea9744503aa5950fe53a8915b41cbaa624c8077f
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-06-13 19:57:20 -0700

    memory alloc fixed

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * _strdup - returns a pointer to a newly allocated space in memory.
  * @str: String to be copyed to the new space.
@@ -12,11 +12,7 @@ char *_strdup(char *str)
 	int i = 0;
 	char *new;
 
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	new = malloc(sizeof(str));
+	new = malloc(strlen(str) + 1);
 	if (new == NULL)
 	{
 		return (NULL);

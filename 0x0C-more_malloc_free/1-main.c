70b0107cc2364cd3761b981bf81293fe405285d6
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-06-18 08:45:33 -0700

    1-string_nconcat.c file updated

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -10,8 +11,9 @@ int main(void)
 {
 	char *concat;
 
-	concat = string_nconcat("Best ", "Best Schoo", 1);
+	concat = string_nconcat("Best ", "School !!!", 2);
 	printf("%s\n", concat);
+	printf("%lu\n", strlen(concat));
 	free(concat);
 	return (0);
 }
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
- * char string_nconca - concatenate two strings
+ * char string_nconcat - concatenate two strings
  * @s1: String 1
  * @s2: String 2
  * @n: The n character to be concatenate.
@@ -13,7 +14,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	/* Declare variables and initialize them to 0 */
 	char *concat;
 	unsigned int len1 = 0;
-	unsigned int i = 0;
+	/* unsigned int i = 0; */
 	unsigned int a = 0, j = 0;
 
 	/* Check if NULL character is passed to strings */
@@ -22,28 +23,30 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
+
 	/* Let gets the size of the string s1 */
-	while (s1[i] != '\0')
+	while (s1[len1] != '\0')
 	{
 		len1++;
-		i++;
 	}
 
 	/* Allocate a required space of memory for the new space */
-	concat = malloc(len1 + n + 1);
+	concat = malloc(strlen(s1) + n + 1);
 	if (concat == NULL)
 	{
 		return (NULL);
 	}
-
-	for (j = 0; j < sizeof(concat) && s1[j] != '\0'; j++)
+	while (s1[j] != '\0')
+	{
 		concat[j] = s1[j];
+		j++;
+	}
 	while (a < n)
 	{
 		concat[j] = s2[a];
 		a++;
 		j++;
 	}
-	concat[j + 1] = '\0';
+	concat[j] = '\0';
 	return (concat);
 }

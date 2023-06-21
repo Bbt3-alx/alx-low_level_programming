a3793fbf676b668414fb0b8b0efc49760af1f9b0
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-06-20 18:37:13 -0700

    Betty codding style : Warning - No description of parameters struct dog was missing

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,17 @@
-struct dog{
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a structure
+ * @name: The name of dog
+ * @age: The age of dog
+ * @owner: The owner name.
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif

a07552467f0e0221ae92766e37bf9e838d54c0ea
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-06-22 18:53:16 -0700

    Hot fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", ch);
-		if (separator != NULL && i < n - 1)		
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 
 	}

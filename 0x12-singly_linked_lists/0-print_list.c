53dd2e91ee1bf693d412568d2cb2d39607573c08
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-07-01 15:57:52 -0700

    A space added between the len and the str

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,13 +16,13 @@ size_t print_list(const list_t *h)
 	{
 		if (temp->str == NULL)
 		{
-			printf("[0](nil)\n");
+			printf("[0] (nil)\n");
 			temp = temp->next;
 			count++;
 		}
 		else
 		{
-			printf("[%d]%s\n", temp->len, temp->str);
+			printf("[%d] %s\n", temp->len, temp->str);
 			temp = temp->next;
 			count++;
 		}

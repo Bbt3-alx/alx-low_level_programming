469d3aea5f75d1151ab66f362dab10745ab6acd8
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-07-02 18:24:36 -0700

    A pre-main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pre_main - Print is executed before the main function.
+ * Return: Nothing.
+ */
+void __attribute__((constructor)) pre_main()
+{
+	printf("You\'re beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

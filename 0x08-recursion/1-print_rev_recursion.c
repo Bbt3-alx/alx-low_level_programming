1c494af3009a7e9bbd9397e280ef992e2f5cede2
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-02 15:12:54 -0700

    1-print_rev_recursion.c updated

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,12 +9,16 @@ void _print_rev_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i] == '\0')
-	{
-		_putchar('\n');
+	if (s == NULL)
 		return;
-	}
+
+	if (s[i] == '\0')
+		return;
+
 	i++;
 	_print_rev_recursion(s + 1);
 	_putchar(s[i]);
+
+	if (s[i - 1] == '\0')
+		_putchar('\n');
 }

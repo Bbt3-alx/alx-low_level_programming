e0119723e0f1b02d4bcc610f1091874df14797f2
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-02 09:54:34 -0700

    0-puts_recursion.c file updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _put_recursion - prints a string, followed by a new line.
+ * _puts_recursion - prints a string, followed by a new line.
  * @s: The string to be printed.
  * Return: Anything.
  */
@@ -9,12 +9,12 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	_putchar(s[i]);
 	if (s[i] == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
+	_putchar(s[i]);
 	i++;
 	_puts_recursion(s + 1);
 

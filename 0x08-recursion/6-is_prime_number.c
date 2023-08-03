4068cc458b081fa63e56dfe890cc52fcb17f628b
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-03 14:25:01 -0700

    Betty codding style ERROR Fixed

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0.
- * @n: The number to be verifyed if prime num or not.
- * Return: returns 1 if the input integer is a prime number, otherwise return 0.
+ * check_prime_number - This functionhelp to find a factor of n.
+ * @x: The base number.
+ * @y: The factor of x.
+ * Return: 1 if the input integer is a prime number, otherwise return 0
  */
 int check_prime_number(int x, int y)
 {
@@ -15,6 +16,12 @@ int check_prime_number(int x, int y)
 
 	return (check_prime_number(x, y + 1));
 }
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0.
+ * @n: The number to be verifyed if prime num or not.
+ * Return: returns 1 if the input integer is a prime number, otherwise return 0
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1)

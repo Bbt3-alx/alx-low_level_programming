247faccef323be811083d9a6a8fc5b5a46faa438
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-03 18:15:08 -0700

    This function check if a string is palindrome or not

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * check_palindrome - Check if 's' is palindrom
+ * @s: String be verified if palindrome or not.
+ * Return: returns 1 if a string is a palindrome and 0 if not.
+ */
+int check_palindrome(char *s, int len)
+{
+	if (len <= 1)
+		return (1);
+	
+	if (*s != s[len - 1])
+		return (0);
+
+	return (check_palindrome(s + 1, len - 2));
+}
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ * @s: The string to be verified if palindrome or not.
+ * Return: returns 1 if a string is a palindrome and 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+
+	if (s == NULL || *s == '\0')
+		return (1);
+	return (check_palindrome(s, len));
+}

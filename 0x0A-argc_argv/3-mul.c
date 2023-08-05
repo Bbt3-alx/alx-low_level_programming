37aa7793ce909fa75964e10baf20c9ddefe9f11c
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-05 09:34:29 -0700

    The function is_valide_number added for verfying is a char is number or not

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+/**
+ * is_valide_number - check if character is a number
+ * @s: Array of string
+ * Return: 1 if the carater is number, 0 if not.
+ */
+int is_valide_number(const char *s)
+{
+	if (*s == '-' || *s == '+')
+		s++;
+	while (*s != '\0')
+	{
+		if (!isdigit(*s))
+			return (0);
+		s++;
+	}
+	return (1);
+}
 /**
  * main - check the code
  * @argc: Argument count
  * @argv: Argument vector
  * Return: Always 0
  */
-
 int main(int argc, char *argv[])
 {
-	int result = 0, i, j;
+	int result = 0, i;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-
-	for (i = 0; i < 2; i++)
+	for (i = 1; i < 2; i++)
 	{
-		for (j = 0; argv[i + 1][j] != '\0'; j++)
+		if (!is_valide_number(argv[i]))
 		{
-			if (!isdigit(argv[i + 1][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("Error\n");
+			return (1);
 		}
 	}
-
 	result = atoi(argv[1]) * atoi(argv[2]);
-
 	printf("%d\n", result);
-
 	return (0);
 }

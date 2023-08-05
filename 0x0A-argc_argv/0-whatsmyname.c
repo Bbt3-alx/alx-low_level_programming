7ac3d479a8b9321577d7e976ddb038f84a710ffb
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-05 04:04:40 -0700

    Betty codding Style fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - check the program
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always 0
+ */
 int main(int argc __attribute__ ((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);

0b4a00232414558bbb0e056e77bb050bb83c1e70
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-06 00:53:36 -0700

    A main file added

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * 
+ * Reyurn: Always 0.
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+
+	return (0);
+}

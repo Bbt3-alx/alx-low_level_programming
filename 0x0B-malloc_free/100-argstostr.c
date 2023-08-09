0a0c8ae516f24566e69e112e08c9191c44e5c9cd
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-09 09:20:48 -0700

    hot fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,18 +14,16 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 	{
-		printf("Imposible");
 		return (NULL);
 	}
-	
-	
+
 	for (i = 0; i < ac; i++)
 	{
 		total_len += strlen(av[i]);
 	}
 	total_len += ac;
 
-	new_str = malloc(total_len + 1);
+	new_str = malloc(total_len * sizeof(*av) + 1);
 	if (new_str == NULL)
 		return (NULL);
 
@@ -40,8 +38,7 @@ char *argstostr(int ac, char **av)
 		if (i < ac - 1)
 			new_str[k++] = ' ';
 	}
-	
+
 	new_str[k] = '\0';
 	return (new_str);
 }
-	

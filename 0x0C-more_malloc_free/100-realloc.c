8e996fbb80344931bdfcbdb53cd9740d1fb7818c
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-11 10:55:20 -0700

    Betty codding style ERROR FIXED

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,7 +6,7 @@
  * @old_size: The preview size of ptr.
  * @new_size: The new_size.
  * Return: A pointer to the new_resized memory.
- */ 
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == 0 && ptr != NULL)
@@ -24,7 +24,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	
 	ptr = realloc(ptr, new_size);
 	if (ptr == NULL)
 		return (NULL);

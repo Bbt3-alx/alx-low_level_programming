d41201d51026adbe8ede73eff558b94f753dca57
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-11 17:59:46 -0700

    Betty codding style

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,7 +23,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	else if (new_size <= old_size)
 		return (ptr);
-	
+
 	ptr = realloc(ptr, new_size);
 	if (ptr == NULL)
 		return (NULL);

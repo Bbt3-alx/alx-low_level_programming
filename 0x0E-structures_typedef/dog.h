8b958efbcf847adc13490964ec9637c712ed45ca
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-14 03:10:16 -0700

    A structure of three members

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - is a structure of 3 memers.
+ * @name:member 1.
+ * @age: member 2.
+ * @owner: member 3.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* MAIN_H */

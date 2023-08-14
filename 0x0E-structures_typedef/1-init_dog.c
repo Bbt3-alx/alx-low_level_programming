6e32ccaa2e4257fab21f3fd6f4845c5d92871651
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-14 03:46:51 -0700

    This function gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-init_dog.c -o b

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initialize a structure of type dog.
+ * @d: Struct to be initialized
+ * @name: member 1.
+ * @age: member 2.
+ * @owner: member 3.
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

e2688e1f285e50bdeb48cab352e23c560c13c4a4
Brehyma Traore <brehymatraore50@gmail.com>
2023-08-14 13:54:34 -0700

    Hot fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,10 +14,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
+	
+	dog->name =strdup( name);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 
-	dog->name = name;
 	dog->age = age;
-	dog->owner = owner;
+
+
+	dog->owner = strdup(owner);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
 
 	return (dog);
 }

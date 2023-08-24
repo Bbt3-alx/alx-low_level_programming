9a62e6c00a3467dcc52fd47e976953aed746a3de
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-08-24 15:38:51 -0700

    All the main function for this project and the first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - Print all the element of a list_t list
+ * @h: Head of the list to be printed
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	const list_t *temp;
+
+	if (h == NULL)
+		return (0);
+
+	temp = h;
+	while (temp != NULL)
+	{
+		printf("[%d] %s\n", temp->len, temp->str);
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}

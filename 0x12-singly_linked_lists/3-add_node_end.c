a84ea2e8f8b2525c93f635c7becee4ee873c126f
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-08-26 03:33:39 -0700

    a function that adds a new node at the end of a list_t list.

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * add_node_end - Adds a node at the end of a list_t list
+ * @head: The head of the list
+ * @str: The string to be added.
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *temp, *new_node;
+
+new_node = malloc(sizeof(list_t));
+
+if (new_node == NULL)
+{
+	return (NULL);
+}
+new_node->str = strdup(str);
+new_node->len = strlen(str);
+new_node->next = NULL;
+
+if (*head == NULL)
+{
+	*head = new_node;
+	return (new_node);
+}
+temp = *head;
+
+while (temp->next != NULL)
+{
+	temp = temp->next;
+}
+temp->next = new_node;
+
+return (new_node);
+}

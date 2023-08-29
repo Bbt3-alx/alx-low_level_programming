4bcfa7bc9befabce2d0003b264b2006ee34099ea
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-08-29 10:51:30 -0700

    In this version, we handle the case when idx is 0. We also increment position after the comparison with idx - 1, which ensures that we insert at the correct position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,19 +14,29 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node = (*head)->next;
+		*head = new_node;
+		return (new_node);
+	}
+
 	temp = *head;
 	while (temp != NULL)
 	{
-		position++;
-		if (position == idx)
+		if (position == idx - 1)
 		{
-			new_node->n = n;
 			new_node->next = temp->next;
 			temp->next = new_node;
 			return (new_node);
 		}
+		position++;
 		temp = temp->next;
 	}
-
+	free(new_node);
 	return (NULL);
 }

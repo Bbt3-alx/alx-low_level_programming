da0ae3057b4fae5c52a50af623bceb4f54a469b6
Bréhyma Traoré <brehymatraore50@gmail.com>
2023-09-05 18:23:57 -0700

    Betty codding style fixed by spliting the code into two function the main() function and the copyFile() function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,36 @@
 #include "main.h"
 /**
+ * copyFile - copies the content of one file to another file.
+ * @source_file: Path to the source file
+ * @destination_file: Path to the destination file
+ * @buffer: The buffer
+ * @file1: The source file name
+ * @file2: The destination file name
+ * Return: Nothing
+ */
+void copyFile(int source_file, int destination_file, char *buffer,
+		char *file1, char *file2)
+{
+	ssize_t bytes_read, bytes_write;
+
+	do {
+		bytes_read = read(source_file, buffer, sizeof(buffer));
+		if (bytes_read == -1)
+		{
+			fprintf(stderr, "Error: Can't read from file %s\n", file1);
+			free(buffer);
+			exit(READ_ERROR);
+		}
+		bytes_write = write(destination_file, buffer, bytes_read);
+		if (bytes_write == -1)
+		{
+			fprintf(stderr, "Error: Can't write to %s\n", file2);
+			exit(WRITE_ERROR);
+		}
+	} while (bytes_read > 0);
+}
+
+/**
  * main - copies the content of a file to another file.
  * @argc: Argument count
  * @argv: Argument vector
@@ -10,9 +41,7 @@ int main(int argc, char **argv)
 {
 	char *buffer, *usage_message;
 	int source_file, destination_file;
-	ssize_t bytes_read, bytes_write;
 
-	/* Check if the number of argument is the correct one */
 	if (argc != 3)
 	{
 		usage_message = "Usage: cp file_from file_to\n";
@@ -20,14 +49,12 @@ int main(int argc, char **argv)
 		exit(USAGE_ERROR);
 	}
 
-	/* Allocate memory for the buffer */
 	buffer = malloc(BUFFER_SIZE);
 	if (buffer == NULL)
 	{
 		exit(EXIT_FAILURE);
 	}
-
-	/* Try to open the file 1, to find out if it exist */
+	/* Open the file 1, to find out if it exist */
 	source_file = open(argv[1], O_RDONLY);
 	if (source_file == -1)
 	{
@@ -35,28 +62,9 @@ int main(int argc, char **argv)
 		free(buffer);
 		exit(READ_ERROR);
 	}
-
-	/* Try to open the file 2 in READ AND WRITE MODE */
 	destination_file = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, PERMISSIONS);
-
 	/* WRITE INTO DESTINATION FILE */
-	do {
-		bytes_read = read(source_file, buffer, sizeof(buffer));
-		if (bytes_read == -1)
-		{
-			fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(READ_ERROR);
-		}
-		bytes_write = write(destination_file, buffer, bytes_read);
-		if (bytes_write == -1)
-		{
-			fprintf(stderr, "Error: Can't write to \n", argv[2]);
-			exit(WRITE_ERROR);
-		}
-	} while (bytes_read > 0);
-
-	/* If one of the file descriptor can't close, exit with code 100 */
+	copyFile(source_file, destination_file, buffer, argv[1], argv[2]);
 	if (close(source_file) == -1)
 	{
 		fprintf(stderr, "Error: Can't close fd %d\n", source_file);
@@ -69,7 +77,6 @@ int main(int argc, char **argv)
 		free(buffer);
 		exit(CLOSE_ERROR);
 	}
-
 	free(buffer);
 	return (0);
 }

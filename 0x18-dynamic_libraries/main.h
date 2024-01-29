16e3b707ea1ffa61f5d6bc7b10a6fd970c5582e1
Brehyma Traore <brehymatraore50@gmail.com>
2024-01-29 09:14:39 -0800

    the dynamic library libdynamic.so

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -3,7 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(void);
+#include <unistd.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -25,4 +26,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif /*MAIN_H*/
